61ae0795a41d0b902e1d6fa6b05a764221ec2776
Charlotte Kunze <45939434+charlyknz@users.noreply.github.com>
2026-04-08 15:05:46 +0200

    update structure for statistic script

--- code/05_NBE_Statistics_Contrasts.R
+++ code/05_NBE_Statistics_Contrasts.R
@@ -1,4 +1,4 @@
-#### Start statistics ####
+#### Start Rscript ####
 # 11.07.2023
 # Helmut Hillebrand & Charlotte Kunze
 
@@ -28,7 +28,7 @@ names(netdiv)
 netdiv$combination<-as.factor(netdiv$combination)
 netdiv$Nfac<-as.factor(netdiv$N)
 
-#### Mixed Model: NBES ~ SR * temperature ####
+##### Mixed Model: NBES ~ SR * temperature #####
 #Step 1. Test possible cases:Fit models using REML (use ML in simplifications)
 M1 = lme(NBE ~ temp*Nfac, random = ~1|combination, method = 'REML', data = netdiv)
 M2 = lme(NBE ~ temp*Nfac, random = ~0+Nfac|combination, method = 'REML', data = netdiv)
@@ -59,7 +59,7 @@ qqnorm(resid(M2, type = "normalized"), main=""); qqline(resid(M2, type = "normal
 anova(M2)
 summary(M2)
 
-#### Singular nested models: combination as random effect nested in N ####
+##### Singular nested models: combination as random effect nested in N #####
 ### 3 singular models instead of 1 as the two fixed effects are correlated --> to decipher underlying mechanisms
 
 #read ZUUR et al. 2008
@@ -137,84 +137,17 @@ incflucM0 <- gls(NBE ~ Nfac, method = 'REML', data = subset(netdiv, temp == 'Inc
 
 rr2::R2_lik(incflucM0)
 
+# keep complete model for now
 
-#### NBES: ANOVA temp combination ####
-aov1<-aov(NBE~temp*combination, netdiv)
-summary(aov1)
 
-ggplot(netdiv, aes(y = NBE, x = as.factor(temp), fill=as.factor(N)))+
-  geom_boxplot()
-
-# test for normality
-hist(netdiv$NBE)
-qqnorm(netdiv$NBE)
-qqline(netdiv$NBE)
-
-#### NBES: T-test ####
+##### NBES: T-test #####
 #test against zero
 test1 <- t.test(netdiv$NBE, mu = 0, alternative = "two.sided")
 test1
 
 
-#### NBES: Presence/absence of species ~NBES ####
-# explore the interaction of temperature and species combinations
-# analyse if presence absence of species affects NBE
-# seperately for each temperature treatment
 
-combiEffect <- netdiv %>%
-  mutate(composition = combination) %>%
-  mutate(A = ifelse(str_detect(combination, 'A'), 1, 0), # for presence 1 for absence 0 
-         D = ifelse(str_detect(combination, 'D'), 1, 0),
-         G = ifelse(str_detect(combination, 'G'), 1, 0),
-         R = ifelse(str_detect(combination, 'R'), 1, 0), 
-         Th = ifelse(str_detect(combination, 'T'), 1, 0)) 
-  
-fluct <- combiEffect %>% filter(temp == 'Fluctuation')
-aov2<-aov(NBE~A+D+G+R+Th, fluct) 
-summary(aov2)
-
-incfluct <- combiEffect %>% filter(temp == 'Increase + Fluctuation')
-aov3<-aov(NBE~A+D+G+R+Th, incfluct)
-summary(aov3)
-
-inc <- combiEffect %>% filter(temp == 'Increase')
-aov4<-aov(NBE~A+D+G+R+Th, inc)
-summary(aov4)
-
-
-### richness levels ###
-library(rstatix )
-fluct2 <- combiEffect %>% filter(temp == 'Fluctuation' & N == 2)
-aov5<-aov(NBE~A+D+G+R+Th, fluct2) 
-anova(aov5)
-rstatix::eta_squared(aov5)
-
-fluct4 <- combiEffect %>% filter(temp == 'Fluctuation' & N == 4)
-aov6<-aov(NBE~A+D+G+R+Th, fluct4) 
-summary(aov6)
-
-incfluct2 <- combiEffect %>% filter(temp == 'Increase + Fluctuation'& N == 2)
-aov7<-aov(NBE~A+D+G+R+Th, incfluct2)
-summary(aov7)
-
-incfluct4 <- combiEffect %>% filter(temp == 'Increase + Fluctuation'& N == 4)
-aov8<-aov(NBE~A+D+G+R+Th, incfluct4)
-summary(aov8)
-
-inc2 <- combiEffect %>% filter(temp == 'Increase'& N == 2)
-aov9<-aov(NBE~A+D+G+R+Th, inc2)
-summary(aov9)
-
-inc4 <- combiEffect %>% filter(temp == 'Increase'& N == 4)
-aov10<-aov(NBE~A+D+G+R+Th, inc4)
-summary(aov10)
-
-summary.table<-tibble()
-summary.table  <- anova(aov10)["Sum Sq"]
-
-cor(inc4[,12:16])
-
-#### NBES: Contrasts ####
+##### NBES: Contrasts #####
 # contrast 1 (con): 2 versus 4 species
 
 netdiv$con1<-NA
@@ -259,26 +192,8 @@ str(HectorRaw)
 HectorRaw$combination<-as.factor(HectorRaw$combination)
 summary(HectorRaw)
 
-#### NBE on F: ANOVA temp, combination ####
-aov1<-aov(NetEffect~temp*combination, HectorRaw)
-summary(aov1)
-TukeyHSD(aov1)
 
-
-ggplot(HectorRaw, aes(y = NetEffect, x = as.factor(temp), fill=as.factor(N)))+
-  geom_boxplot()
-
-# test for normality
-fligner.test(NetEffect~interaction(temp,combination), HectorRaw)
-
-plot(resid(aov1))
-hist(resid(aov1))
-hist(((HectorRaw$NetEffect)))
-qqnorm(HectorRaw$NetEffect)
-qqline(HectorRaw$NetEffect)
-
-
-#### Mixed Model: NBEF ~ SR * temperature ####
+##### Mixed Model: NBEF ~ SR * temperature #####
 #Step 1. Test possible cases:Fit models using REML (use ML in simplifications)
 H1 = lme(NetEffect ~ temp*N, random = ~1|combination, method = 'REML', data = HectorRaw)
 H2 = lme(NetEffect ~ temp*N, random = ~0+N|combination, method = 'REML', data = HectorRaw)
@@ -309,13 +224,13 @@ qqnorm(resid(H1, type = "normalized"), main=""); qqline(resid(H1, type = "normal
 anova(H1)
 summary(H1)
 
-#### NBE on F: T-Test ####
+##### NBE on F: T-Test #####
 #test against zero
 test2<-t.test(HectorRaw$NetEffect, mu = 0, alternative = "two.sided")
 test2
 
 
-#### NBES - NBE on F: Correlation ####
+##### NBES - NBE on F: Correlation #####
 Corr.data <- netdiv %>%
   select(combination, temp, rep, NBE, N) %>%
   mutate(N = as.factor(N)) %>%
@@ -335,3 +250,93 @@ ggscatter(Corr.data, x = 'NetEffect', y='NBE', add = 'reg.line', col = 'temp',co
 
 ggsave(plot = last_plot(), file = here('output/Correlation_NBE_NBES.png'), width = 6, height = 5)
 
+
+#### Additional statistical tests ####
+
+##### NBES: ANOVA temp combination #####
+aov1<-aov(NBE~temp*combination, netdiv)
+summary(aov1)
+
+ggplot(netdiv, aes(y = NBE, x = as.factor(temp), fill=as.factor(N)))+
+  geom_boxplot()
+
+# test for normality
+hist(netdiv$NBE)
+qqnorm(netdiv$NBE)
+qqline(netdiv$NBE)
+
+##### NBE on F: ANOVA temp, combination #####
+aov1<-aov(NetEffect~temp*combination, HectorRaw)
+summary(aov1)
+TukeyHSD(aov1)
+
+
+ggplot(HectorRaw, aes(y = NetEffect, x = as.factor(temp), fill=as.factor(N)))+
+  geom_boxplot()
+
+# test for normality
+fligner.test(NetEffect~interaction(temp,combination), HectorRaw)
+
+plot(resid(aov1))
+hist(resid(aov1))
+hist(((HectorRaw$NetEffect)))
+qqnorm(HectorRaw$NetEffect)
+qqline(HectorRaw$NetEffect)
+
+##### NBES: Presence/absence of species ~NBES #####
+# explore the interaction of temperature and species combinations
+# analyse if presence absence of species affects NBE
+# separately for each temperature treatment
+
+combiEffect <- netdiv %>%
+  mutate(composition = combination) %>%
+  mutate(A = ifelse(str_detect(combination, 'A'), 1, 0), # for presence 1 for absence 0 
+         D = ifelse(str_detect(combination, 'D'), 1, 0),
+         G = ifelse(str_detect(combination, 'G'), 1, 0),
+         R = ifelse(str_detect(combination, 'R'), 1, 0), 
+         Th = ifelse(str_detect(combination, 'T'), 1, 0)) 
+
+fluct <- combiEffect %>% filter(temp == 'Fluctuation')
+aov2<-aov(NBE~A+D+G+R+Th, fluct) 
+summary(aov2)
+
+incfluct <- combiEffect %>% filter(temp == 'Increase + Fluctuation')
+aov3<-aov(NBE~A+D+G+R+Th, incfluct)
+summary(aov3)
+
+inc <- combiEffect %>% filter(temp == 'Increase')
+aov4<-aov(NBE~A+D+G+R+Th, inc)
+summary(aov4)
+
+
+### richness levels ###
+library(rstatix )
+fluct2 <- combiEffect %>% filter(temp == 'Fluctuation' & N == 2)
+aov5<-aov(NBE~A+D+G+R+Th, fluct2) 
+anova(aov5)
+rstatix::eta_squared(aov5)
+
+fluct4 <- combiEffect %>% filter(temp == 'Fluctuation' & N == 4)
+aov6<-aov(NBE~A+D+G+R+Th, fluct4) 
+summary(aov6)
+
+incfluct2 <- combiEffect %>% filter(temp == 'Increase + Fluctuation'& N == 2)
+aov7<-aov(NBE~A+D+G+R+Th, incfluct2)
+summary(aov7)
+
+incfluct4 <- combiEffect %>% filter(temp == 'Increase + Fluctuation'& N == 4)
+aov8<-aov(NBE~A+D+G+R+Th, incfluct4)
+summary(aov8)
+
+inc2 <- combiEffect %>% filter(temp == 'Increase'& N == 2)
+aov9<-aov(NBE~A+D+G+R+Th, inc2)
+summary(aov9)
+
+inc4 <- combiEffect %>% filter(temp == 'Increase'& N == 4)
+aov10<-aov(NBE~A+D+G+R+Th, inc4)
+summary(aov10)
+
+summary.table<-tibble()
+summary.table  <- anova(aov10)["Sum Sq"]
+
+cor(inc4[,12:16])
